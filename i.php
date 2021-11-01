4ee874d1edc6401f873f6941f79396bea0f702ec
kva3050344 <70027152+kva3050344@users.noreply.github.com>
2021-11-01 15:13:32 +0200

    Add files via upload

---
+++ i.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+	<title>Сайт Владимира</title>
+	<link rel="stylesheet" href="style.min.css">
+</head>
+<body>
+	<div class="container">
+	<div class="">
+		<div class="world">
+
+			<div class="part">
+				<h2 class="part__title">Америка</h2>
+				<div class="part__bg">
+					<div class="part__flag">
+						<img src="/img/flag.png" alt="флаг США">
+						<h3>США</h3>
+					</div>
+					<h4 class="part__citytitle">Города</h4>
+					<ul class="part__citylist">
+						<li><img src="/img/f1.png" alt="флаг Нью-Йорка"> Нью-Йорк</li>
+						<li><img src="/img/f2.png" alt="флаг Чикаго"> Чикаго</li>
+					</ul>
+				</div>
+			</div>
+			<div class="part">
+				<h2 class="part__title">Европа</h2>
+				<div class="part__bg">
+					<div class="part__flag">
+						<img src="/img/flag.png" alt="флаг США">
+						<h3>Франция</h3>
+					</div>
+					<h4 class="part__citytitle">Города</h4>
+					<ul class="part__citylist">
+						<li><img src="/img/f1.png" alt="флаг Нью-Йорка"> Париж</li>
+						<li><img src="/img/f2.png" alt="флаг Чикаго"> Леон</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="part">
+				<h2 class="part__title">Азия</h2>
+				<div class="part__bg">
+					<div class="part__flag">
+						<img src="/img/flag.png" alt="флаг США">
+						<h3>Китай</h3>
+					</div>
+					<h4 class="part__citytitle">Города</h4>
+					<ul class="part__citylist">
+						<li><img src="/img/f1.png" alt="флаг Нью-Йорка"> Пекин</li>
+						<li><img src="/img/f2.png" alt="флаг Чикаго"> Тянцзынь</li>
+					</ul>
+				</div>
+			</div>
+
+
+			<div class="part">
+				
+			</div>
+
+			<div class="part">
+				<h2 class="part__title">Африка</h2>
+				<div class="part__bg">
+					<div class="part__flag">
+						<img src="/img/flag.png" alt="флаг США">
+						<h3>ЮАР</h3>
+					</div>
+					<h4 class="part__citytitle">Города</h4>
+					<ul class="part__citylist">
+						<li><img src="/img/f1.png" alt="флаг Нью-Йорка"> Дурбан</li>
+						<li><img src="/img/f2.png" alt="флаг Чикаго"> Йоханнесбург</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="part">
+				<h2 class="part__title">Австралия</h2>
+				<div class="part__bg">
+					<div class="part__flag">
+						<img src="/img/flag.png" alt="флаг США">
+						<h3>Австралия</h3>
+					</div>
+					<h4 class="part__citytitle">Города</h4>
+					<ul class="part__citylist">
+						<li><img src="/img/f1.png" alt="флаг Нью-Йорка"> Канберра</li>
+						<li><img src="/img/f2.png" alt="флаг Чикаго"> Сидней</li>
+					</ul>
+				</div>
+			</div>			
+</div>
+</div>
+	</div>
+</body>
+</html>
